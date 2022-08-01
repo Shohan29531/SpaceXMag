a81accc63dc1bcc5cd95e2ed0e64a27fd6c988c2
touhid shohan <touhidul.shohan@gmail.com>
2022-07-31 22:15:47 -0400

    cursor adjustment in progress

--- mouse_cursor.py
+++ mouse_cursor.py
@@ -1,45 +1,28 @@
-# Import module 
-from tkinter import *
-  
-# Create object 
-root = Tk()
-  
-# Adjust size 
-root.geometry("400x400")
-  
-# Add image file
-bg = PhotoImage(file = "Your_img.png")
-  
-# Create Canvas
-canvas1 = Canvas( root, width = 400,
-                 height = 400)
-  
-canvas1.pack(fill = "both", expand = True)
-  
-# Display image
-canvas1.create_image( 0, 0, image = bg, 
-                     anchor = "nw")
-  
-# Add Text
-canvas1.create_text( 200, 250, text = "Welcome")
-  
-# Create Buttons
-button1 = Button( root, text = "Exit")
-button3 = Button( root, text = "Start")
-button2 = Button( root, text = "Reset")
-  
-# Display Buttons
-button1_canvas = canvas1.create_window( 100, 10, 
-                                       anchor = "nw",
-                                       window = button1)
+import tkinter as tk
+from PIL import Image, ImageTk
 
+IMG1 = "output_1.jpg"
+IMG2 = "output_2.jpg"
 
-button2_canvas = canvas1.create_window( 100, 40,
-                                       anchor = "nw",
-                                       window = button2)
-  
-button3_canvas = canvas1.create_window( 100, 70, anchor = "nw",
-                                       window = button3)
-  
-# Execute tkinter
-root.mainloop()                                       
+class ExampleApp(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.x = self.y = 0
+        self.canvas = tk.Canvas(self, width=1080, height=1920, cursor="cross")
+        self.canvas.pack(side="top", fill="both", expand=True)
+
+        self.image = Image.open(IMG1).resize((400, 400))
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image((0, 0), anchor="nw", image=self.photo)
+
+        self.bind('<Button-1>', self.update)
+
+    def update(self, event):
+        self.image = Image.open(IMG2).resize((400, 400))
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.canvas.create_image((0, 0), anchor="nw", image=self.photo)
+
+if __name__ == "__main__":
+    app = ExampleApp()
+    app.mainloop()

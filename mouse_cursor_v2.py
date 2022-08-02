bcddb790b248b2b5ef3c554dc73284aecfd086a0
Touhid Shohan <touhidul.shohan@gmail.com>
2022-08-01 21:01:22 -0400

    zoom in and out functional

--- mouse_cursor_v2.py
+++ mouse_cursor_v2.py
@@ -5,8 +5,12 @@ import custom_fisheye_effect_opencv as F
 
 scale_factor = 0.6
 current_fisheye_radius = 150
-d = [ 1.5, 3.0, 4.5, 6 ]
+d = [ 1.5, 2, 2.5, 3.0]
 xw = [0.6, 0.4, 0.2, 0]
+
+d_index = 0
+xw_index = 0
+
 
 img = cv2.imread('Output.jpg')
 
@@ -28,14 +32,38 @@ def mouse_events( event, x, y, flags, param ):
         # LB = 'Left Button'
         # cv2.putText( img, LB, (x, y), font, 1, (255, 255, 0), 2 )
         
-        new_img = F.apply_fisheye_effect( img_file = img, fisheye_focus = (x, y), fisheye_radius = int( current_fisheye_radius * scale_factor ) )
+
+        ## zoom in
+        global current_fisheye_radius, d_index, xw_index
+        current_fisheye_radius += 25
+
+        if ( current_fisheye_radius >= 250 ):
+            current_fisheye_radius = 250
+
+        d_index = d_index + 1
+
+        if( d_index == len(d) ):
+            d_index = d_index -1
+
+        new_img = F.apply_fisheye_effect( img_file = img, fisheye_focus = (x, y), fisheye_radius = int( current_fisheye_radius * scale_factor ), d = d[ d_index ] )
         new_img = cv2.resize( new_img, ( int( dim_x * scale_factor ), int( dim_y * scale_factor ) ) )
         cv2.imshow( 'image', new_img )  
     
     ## Right button click    
     elif( event == cv2.EVENT_RBUTTONDOWN ):
         
-        new_img = F.apply_fisheye_effect( img_file = img, fisheye_focus = (x, y), fisheye_radius = int( current_fisheye_radius * scale_factor ) )
+        # global current_fisheye_radius, d_index, xw_index
+        current_fisheye_radius -= 25
+
+        if ( current_fisheye_radius < 150 ):
+            current_fisheye_radius = 150
+
+        d_index = d_index - 1
+
+        if( d_index == -1 ):
+            d_index = 0
+
+        new_img = F.apply_fisheye_effect( img_file = img, fisheye_focus = (x, y), fisheye_radius = int( current_fisheye_radius * scale_factor ), d = d[ d_index ] )
         new_img = cv2.resize( new_img, ( int( dim_x * scale_factor ), int( dim_y * scale_factor ) ) )
         cv2.imshow( 'image', new_img )  
 
@@ -52,7 +80,9 @@ def mouse_events( event, x, y, flags, param ):
               
     ## Mouse cursor hover    
     elif( event == EVENT_MOUSEMOVE ):
-        return
+        new_img = F.apply_fisheye_effect( img_file = img, fisheye_focus = (x, y), fisheye_radius = int( current_fisheye_radius * scale_factor ) )
+        new_img = cv2.resize( new_img, ( int( dim_x * scale_factor ), int( dim_y * scale_factor ) ) )
+        cv2.imshow( 'image', new_img )  
 
 
 

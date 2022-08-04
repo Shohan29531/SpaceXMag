3ef2a57dbfe82532cd9b03491233965a1ae52e16
Touhid Shohan <touhidul.shohan@gmail.com>
2022-08-04 15:49:01 -0400

    computation factor introduced, previous performance times restored

--- fisheye_user_controlled.py
+++ fisheye_user_controlled.py
@@ -1,12 +1,18 @@
 import cv2
-from cv2 import EVENT_MOUSEMOVE
-from cv2 import EVENT_FLAG_CTRLKEY
 
 import custom_fisheye_effect_opencv as F
 
 
-scale_factor = 0.5
-current_fisheye_radius = 150
+scale_factor_display = 0.5
+scale_factor_computation = 0.5
+current_fisheye_radius = 152
+
+
+min_fisheye_radius = 152
+max_fisheye_radius = 500
+step_size = ( max_fisheye_radius - min_fisheye_radius) / 4
+
+
 d = [ 1.5, 2, 2.5, 3.0]
 xw = [0.6, 0.4, 0.2, 0]
 lens_shapes = ['circular', 'elliptical', 'rectangular']
@@ -20,8 +26,10 @@ dim_x, dim_y = img.shape[1], img.shape[0]
 
 
 cv2.namedWindow("image", cv2.WINDOW_GUI_NORMAL)
+
+img = cv2.resize( img, ( int( dim_x * scale_factor_computation ), int( dim_y * scale_factor_computation ) ) )
 cv2.imshow( 'image', img )
-cv2.resizeWindow('image', ( int( dim_x * scale_factor ), int( dim_y * scale_factor ) ))
+cv2.resizeWindow('image', ( int( dim_x * scale_factor_display ), int( dim_y * scale_factor_display ) ))
 
 pos_x = 0
 pos_y = 0
@@ -29,13 +37,13 @@ pos_y = 0
 def render_new_image(img, x, y, lens_shape = 'circular'):
 
     if ( lens_shape == 'circular'):
-        new_img = F.apply_fisheye_effect_circular( img_file = img, fisheye_focus = (x, y), fisheye_radius = current_fisheye_radius, d = d[ d_index ] )
+        new_img = F.apply_fisheye_effect_circular( img_file = img, fisheye_focus = (x, y), fisheye_radius = int( current_fisheye_radius * scale_factor_computation ), d = d[ d_index ] )
 
     elif ( lens_shape == 'elliptical'):
-        new_img = F.apply_fisheye_effect_elliptical( img_file = img, fisheye_focus = (x, y), fisheye_radius = current_fisheye_radius, d = d[ d_index ] )   
+        new_img = F.apply_fisheye_effect_elliptical( img_file = img, fisheye_focus = (x, y), fisheye_radius = int( current_fisheye_radius * scale_factor_computation ), d = d[ d_index ] )   
 
     else:
-        new_img = F.apply_fisheye_effect_rectangular( img_file = img, fisheye_focus = (x, y), fisheye_radius = current_fisheye_radius, d = d[ d_index ] )    
+        new_img = F.apply_fisheye_effect_rectangular( img_file = img, fisheye_focus = (x, y), fisheye_radius = int( current_fisheye_radius * scale_factor_computation ), d = d[ d_index ] )    
     
     cv2.imshow( 'image', new_img )  
 
@@ -54,10 +62,10 @@ def mouse_events( event, x, y, flags, param ):
 
         print("zoom in")
         
-        current_fisheye_radius += 25
+        current_fisheye_radius += step_size
 
-        if ( current_fisheye_radius >= 250 ):
-            current_fisheye_radius = 250
+        if ( current_fisheye_radius >= max_fisheye_radius ):
+            current_fisheye_radius = max_fisheye_radius
 
         d_index = d_index + 1
 
@@ -72,10 +80,10 @@ def mouse_events( event, x, y, flags, param ):
         
         print("zoom out")
 
-        current_fisheye_radius -= 25
+        current_fisheye_radius -= step_size
 
-        if ( current_fisheye_radius < 150 ):
-            current_fisheye_radius = 150
+        if ( current_fisheye_radius < min_fisheye_radius ):
+            current_fisheye_radius = min_fisheye_radius
 
         d_index = d_index - 1
 
